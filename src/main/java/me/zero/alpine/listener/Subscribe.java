88fb1afb210e48d15ee323535bb641ae4b88434d
Brady <zeromemesdev@gmail.com>
2023-05-18 23:40:34 -0500

    Update `@Subscribe` documentation to reflect discovery changes

--- src/main/java/me/zero/alpine/listener/Subscribe.java
+++ src/main/java/me/zero/alpine/listener/Subscribe.java
@@ -1,14 +1,42 @@
 package me.zero.alpine.listener;
 
+import me.zero.alpine.bus.EventBus;
+import me.zero.alpine.listener.discovery.ListenerDiscoveryStrategy;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Used to mark {@link Listener} fields to be targeted during an {@link Subscriber}'s {@link Listener} field
- * discovery. {@link Listener} fields which are not annotated with {@link Subscribe} will not be registered for
- * invocation by their target event.
+ * Used to mark instance members of classes implementing {@link Subscriber} as recognizable during listener discovery.
+ * <p>
+ * When an {@link EventBus} is using the built-in {@code subscribeFields()} discovery strategy, fields annotated with
+ * {@link Subscribe} must be of the type {@link Listener} and have an explicit type parameter.
+ *
+ * <pre> {@code
+ * public class EventHandler implements Subscriber {
+ *   @Subscribe
+ *   public Listener<Event> eventListener = new Listener<>(event -> {
+ *     // process 'event'
+ *   });
+ * }
+ * }</pre>
+ *
+ * When an {@link EventBus} is using the built-in {@code subscribeMethods()} discovery strategy, methods annotated with
+ * {@link Subscribe} must return {@code void} and have a single parameter which is the target event type.
+ *
+ * <pre> {@code
+ * public class EventHandler implements Subscriber {
+ *   @Subscribe
+ *   public onEvent(Event event) {
+ *     // process 'event'
+ *   }
+ * }
+ * }</pre>
+ *
+ * @see ListenerDiscoveryStrategy#subscribeFields()
+ * @see ListenerDiscoveryStrategy#subscribeMethods()
  *
  * @author Brady
  * @since 1.2
