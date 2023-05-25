a2b71a461bd2591ddde461bea599486e8194c37c
Brady <zeromemesdev@gmail.com>
2023-05-24 23:31:11 -0500

    Fix tests due to target restriction

--- src/test/java/me/zero/alpine/bus/SuperListenersTest.java
+++ src/test/java/me/zero/alpine/bus/SuperListenersTest.java
@@ -4,11 +4,6 @@ import me.zero.alpine.listener.Listener;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-
 import static org.mockito.Mockito.*;
 
 /**
@@ -31,9 +26,7 @@ public class SuperListenersTest {
     void testNone() {
         final Listener<Object> listener = mock(Listener.class);
         when(listener.getTarget()).thenReturn(Object.class);
-
         bus.subscribe(listener);
-        verify(listener, never()).accept(any());
 
         final Object event = new Object();
         bus.post(event);
@@ -44,9 +37,7 @@ public class SuperListenersTest {
     void testSingle() {
         final Listener<Object> listener = mock(Listener.class);
         when(listener.getTarget()).thenReturn(Object.class);
-
         bus.subscribe(listener);
-        verify(listener, never()).accept(any());
 
         // Class with one parent (Object)
         class Event {}
@@ -58,34 +49,31 @@ public class SuperListenersTest {
 
     @Test
     @SuppressWarnings("rawtypes")
-    void testMulti() {
-        final Listener listenerA = mock(Listener.class);
-        when(listenerA.getTarget()).thenReturn(List.class);
+    void testDiamond() {
+        final Listener listener = mock(Listener.class);
+        when(listener.getTarget()).thenReturn(Diamond.D.class);
+        bus.subscribe(listener);
 
-        final Listener listenerB = mock(Listener.class);
-        when(listenerB.getTarget()).thenReturn(Collection.class);
+        // Ensure that the listener is called once despite 2 inheritance paths
+        //  A -> B -> D
+        //  A -> C -> D
+        final Diamond.A event = new Diamond.A();
+        bus.post(event);
+        verify(listener, times(1)).accept(event);
+    }
 
-        bus.subscribe(listenerA);
-        bus.subscribe(listenerB);
-        verify(listenerA, never()).accept(any());
-        verify(listenerB, never()).accept(any());
-
-        // Ensure that the listeners aren't called twice due to 2 inheritance paths
-        //  ArrayList -> List
-        //  ArrayList -> AbstractList -> List
-        final ArrayList list = new ArrayList();
-        bus.post(list);
-        verify(listenerA, times(1)).accept(list);
-        verify(listenerB, times(1)).accept(list);
-
-        // Collection which doesn't implement List
-        HashSet set = new HashSet();
-        bus.post(set);
-        verify(listenerA, never()).accept(set);
-        verify(listenerB, times(1)).accept(set);
-
-        // Total # of callback invocations
-        verify(listenerA, times(1)).accept(any());
-        verify(listenerB, times(2)).accept(any());
+    static class Diamond {
+        //     A
+        //    / \
+        //   /   \
+        //  B     C
+        //   \   /
+        //    \ /
+        //     D
+        static class A implements B, C {}
+        // Local interfaces not supported until Java 16
+        interface B extends D {}
+        interface C extends D {}
+        interface D {}
     }
 }
