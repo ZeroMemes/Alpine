2c645a683f5bd1c8feb4e10158abf4372901c17f
Brady <zeromemesdev@gmail.com>
2023-06-05 18:43:51 -0500

    This was not meant to be marked `public`

--- src/main/java/me/zero/alpine/listener/DefaultListenerListFactory.java
+++ src/main/java/me/zero/alpine/listener/DefaultListenerListFactory.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
  * @author Brady
  * @since 3.0.0
  */
-public enum DefaultListenerListFactory implements ListenerListFactory {
+enum DefaultListenerListFactory implements ListenerListFactory {
     INSTANCE;
 
     @Override
