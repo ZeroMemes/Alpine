9e40a21d8ece2d619050252807403ac20d44a0a4
Brady <zeromemesdev@gmail.com>
2023-06-07 20:21:08 -0500

    Fix `Listener` order bug in `ListenerArrayList`

--- src/main/java/me/zero/alpine/listener/ListenerArrayList.java
+++ src/main/java/me/zero/alpine/listener/ListenerArrayList.java
@@ -1,19 +1,23 @@
 package me.zero.alpine.listener;
 
-import it.unimi.dsi.fastutil.objects.ObjectArraySet;
 import me.zero.alpine.event.dispatch.EventDispatcher;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
+ * An implementation of {@link ListenerList} which is backed by
+ *
  * @author Brady
  * @since 3.0.0
  */
 public final class ListenerArrayList<T> implements ListenerList<T> {
 
-    private final ObjectArraySet<Listener<T>> backing;
+    private final ArrayList<Listener<T>> backing;
 
     public ListenerArrayList() {
-        this.backing = new ObjectArraySet<>();
+        this.backing = new ArrayList<>();
     }
 
     @Override
@@ -23,7 +27,15 @@ public final class ListenerArrayList<T> implements ListenerList<T> {
 
     @Override
     public boolean add(@NotNull Listener<T> listener) {
-        return this.backing.add(listener);
+        if (this.backing.contains(listener)) {
+            return false;
+        }
+        int index = Collections.binarySearch(this.backing, listener);
+        if (index < 0) {
+            index = -index - 1;
+        }
+        this.backing.add(index, listener);
+        return true;
     }
 
     @Override
