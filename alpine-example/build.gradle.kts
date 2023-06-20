7d4d688012acdc6fcf8686e79219fb96099b7395
Brady <zeromemesdev@gmail.com>
2023-06-20 17:21:40 -0500

    Use shorthand for including Kotlin

--- alpine-example/build.gradle.kts
+++ alpine-example/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.8.21"
+    kotlin("jvm") version "1.8.21"
 }
 
 java {
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     implementation(rootProject)
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<KotlinCompile>().configureEach {
